ab75621497a42901383ab630a4d3f5c07f6f5adb
François Michonneau <francois.michonneau@gmail.com>
2017-08-06 12:00:36 -0700

    fix #2: add data for :jp:

--- gapminder-plots.R
+++ gapminder-plots.R
@@ -8,3 +8,9 @@ ggplot(gapminder, aes(x = gdpPercap, y = lifeExp)) +
   geom_point(aes(color = continent)) +
   scale_x_log10() +
   geom_smooth(method="lm", color="red")
+
+japan_gapminder <- gapminder[gapminder$country == "Japan", ]
+
+ggplot(japan_gapminder, aes(x = year, y = lifeExp)) +
+  geom_line(color = "coral") +
+  ylab("Life expectancy") + xlab("Year")
